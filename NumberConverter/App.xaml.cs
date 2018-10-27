7b91b72e33f8f863af5aa7113e766f04bcc2c325
Larcer <galeckii1998@gmail.com>
2018-10-27 20:26:59 +0300

    Add content window

--- NumberConverter/App.xaml.cs
+++ NumberConverter/App.xaml.cs
@@ -7,5 +7,10 @@ namespace Nameless.NumberConverter
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+        }
     }
 }
