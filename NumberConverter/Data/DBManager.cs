2d7115a2c1b6d8452f2c43fb59a4c155069eb077
Larcer <galeckii1998@gmail.com>
2018-10-27 20:32:58 +0300

    Add data layer

---
+++ NumberConverter/Data/DBManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Nameless.NumberConverter.Models;
+using Nameless.NumberConverter.Tools;
+
+namespace Nameless.NumberConverter.Data
+{
+    public class DBManager : SingletonBase<DBManager>
+    {
+        private readonly IDictionary<string, User> _users = new Dictionary<string, User>();
+
+        private DBManager()
+        {
+
+        }
+
+        public bool UserExists(string login)
+        {
+            return _users.ContainsKey(login);
+        }
+
+        public User GetUserByLogin(string login)
+        {
+            return _users.ContainsKey(login) ? _users[login] : null;
+        }
+
+        public void AddUser(User user)
+        {
+            _users.Add(user.Login, user);
+        }
+    }
+}
---
+++ NumberConverter/Tools/SingletonBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Nameless.NumberConverter.Tools
+{
+    public abstract class SingletonBase<T> where T : class
+    {
+        private static readonly Lazy<T> sInstance = new Lazy<T>(ConstructInstance);
+
+        public static T Instance => sInstance.Value;
+
+        private static T ConstructInstance()
+        {
+            Type type = typeof(T);
+            ConstructorInfo ci = type.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic,
+                null, Type.EmptyTypes, null);
+
+            Debug.Assert(ci != null);
+
+            return ci.Invoke(null) as T;
+        }
+    }
+}
